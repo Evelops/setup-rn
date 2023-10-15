2a82d23a73505cd9e68c2c59df98890cc6411cf5
Evelops <co6747@naver.com>
2023-10-15 16:23:50 +0900

    chore:rename app

--- ios/billRnAppTests/billRnAppTests.m
+++ ios/billRnAppTests/billRnAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface billAppTests : XCTestCase
+@interface billRnAppTests : XCTestCase
 
 @end
 
-@implementation billAppTests
+@implementation billRnAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
